9e066cef78d41037603401777923dc03094fbc4d
gjm <gmccluskey@yahoo.com>
2017-09-26 07:05:53 -0500

    New

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,17 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
+#include "spline.h"
+
+int lane = 1;
+double ref_vel = 0.0;
 
 using namespace std;
+
+const double safeDistFront=25.0;
+const double safeDistBack=20.0;
+const double safeDistBackFuture=40.0;
+const double safeDistDetectionVal=30.0;
 
 // for convenience
 using json = nlohmann::json;
@@ -159,6 +168,148 @@ vector<double> getXY(double s, double d, const vector<double> &maps_s, const vec
 
 }
 
+int cost_function1(vector<vector<double>> sensor_fusion, double current_s, double car_speed, int current_lane)
+{
+	
+	//Might want to change lane so check out our options
+	long kcost = 0; //keep lane cost
+	long rcost = 0; //right lane cost
+	long lcost = 0; //left lane cost
+	int othercar_lane;
+	int lane_to_return = current_lane;
+	double lclosestcar_s = 99999;
+	double rclosestcar_s = 99999;
+	double lclosestcar_s_back = -99999;
+	double rclosestcar_s_back = -99999;
+	double lclosestcar_v_back = 0.0;
+	double rclosestcar_v_back = 0.0;
+
+
+	//For each othercar in left lane:
+	for( int i = 0; i < sensor_fusion.size(); i++) {
+		//get lane the this other car is in
+		float d = sensor_fusion[i][6];
+		double check_car_s = sensor_fusion[i][5]; //s position of car in my lane
+		double check_car_vx = sensor_fusion[i][3];//velocity x
+		double check_car_vy = sensor_fusion[i][4];//velocity y
+
+		if( d < 4.0 ) 
+			othercar_lane = 0; //left lane
+		else if(d >= 4.0 && d < 8.0 )
+			othercar_lane = 1; //center lane
+		else
+			othercar_lane = 2; //right lane
+
+		//In left most lane already
+		if( current_lane == 0 )
+			lcost+=999;
+
+		//In right most lane already
+		if( current_lane == 2 )
+			rcost+=999;
+
+		//relative left lane only...
+		if( othercar_lane == current_lane - 1 ) {
+			if( check_car_s > (current_s - safeDistBack) && check_car_s < (current_s + safeDistFront) ) //Unsafe range
+				lcost += 999;
+			else
+				kcost += 100;
+						        
+		}
+		//relative right lane only...
+		else if( othercar_lane == current_lane + 1 ) {
+			if( (check_car_s > (current_s - safeDistBack) && check_car_s < (current_s + safeDistFront)) || current_lane == 2 ) //Unsafe range
+				rcost += 999;
+			else
+				kcost += 100;
+						        
+		}
+
+		//Find closest distances of nearest-car-in-front and for left and right lanes which may be used to determine which lane to go to		
+		if( othercar_lane == (current_lane - 1) )  { //left
+			if( current_s < check_car_s && check_car_s < lclosestcar_s )
+				lclosestcar_s = check_car_s;
+
+		}
+		else if( othercar_lane == (current_lane + 1) )  { //right
+			if( current_s < check_car_s && check_car_s < rclosestcar_s )
+				rclosestcar_s = check_car_s;
+		}
+
+		//Find closest distances and velocities of nearest-car-in-back and for left and right lanes which may be used to determine which lane to go to	
+		if( othercar_lane == (current_lane - 1) )  { //left
+			if( current_s > check_car_s && check_car_s > lclosestcar_s_back ) {
+				lclosestcar_s_back = check_car_s;
+				lclosestcar_v_back = sqrt(check_car_vx*check_car_vx + check_car_vy*check_car_vy);
+			}
+		}
+		else if( othercar_lane == (current_lane + 1) )  { //right
+			if( current_s > check_car_s && check_car_s > rclosestcar_s_back ) {
+				rclosestcar_s_back = check_car_s;
+			    rclosestcar_v_back = sqrt(check_car_vx*check_car_vx + check_car_vy*check_car_vy);
+			}
+		}
+
+		//
+		
+
+	}//for
+
+	//cout << "keep=" << kcost << " lcost=" << lcost << " rcost=" << rcost << endl;
+
+	//Check for best if have a choice of left or right...
+	if( current_lane == 1 ) { //Only revelent for center lane
+	 
+      if( rclosestcar_s > lclosestcar_s ) {
+		  lcost+=200;
+		  //cout << "Best to go right" << endl;
+	  }
+	  else {
+		  rcost+=200;
+		  //cout << "Best to go left" << endl;
+	  }
+	}
+
+	//Check to see cars in other lanes are on a collision course from behind
+	double future_s = current_s + (50.0*.02*car_speed); //project s 50 points in future in left lane
+	if( othercar_lane == current_lane - 1 ) { //left
+		//project closest car in left lane
+		double check_future_s = lclosestcar_s_back + (50.0*.02*lclosestcar_v_back); //project s 50 points in future in left lane
+		
+		if( check_future_s > (future_s - safeDistBackFuture) ) { //Unsafe range
+			lcost += 999; //dangerous
+			//cout << "Accelerating carin left lane behind" << endl;
+		}
+		else
+			kcost += 100;					        
+	}
+	if( othercar_lane == current_lane + 1 ) { //right
+		//project closest car in right lane
+		double check_future_s = rclosestcar_s_back + (50.0*.02*rclosestcar_v_back); //project s 50 points in future in left lane
+		
+		if( check_future_s > (future_s - safeDistBackFuture) ) { //Unsafe range
+			rcost += 999; //dangerous
+		    //cout << "Accelerating car in right lane behind" << endl;
+		}
+		else
+			kcost += 100;					        
+	}
+
+
+
+	//Determine which lane is best based on cost...
+	if( kcost > rcost || kcost > lcost ) {
+		if( lcost <= rcost ) 								
+			lane_to_return--; //Change to left lane
+		else 
+			lane_to_return++; //Change to right lane
+	}
+
+	//else leave lanes as is	
+
+	return lane_to_return;
+}
+
 int main() {
   uWS::Hub h;
 
@@ -168,6 +319,8 @@ int main() {
   vector<double> map_waypoints_s;
   vector<double> map_waypoints_dx;
   vector<double> map_waypoints_dy;
+
+  
 
   // Waypoint map to read from
   string map_file_ = "../data/highway_map.csv";
@@ -195,6 +348,10 @@ int main() {
   	map_waypoints_dx.push_back(d_x);
   	map_waypoints_dy.push_back(d_y);
   }
+
+  //int lane = 1;
+  
+
 
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
@@ -233,13 +390,212 @@ int main() {
           	// Sensor Fusion Data, a list of all other cars on the same side of the road.
           	auto sensor_fusion = j[1]["sensor_fusion"];
 
-          	json msgJson;
+			int prev_size = previous_path_x.size();
+
+			if( prev_size > 0 ) {
+				car_s = end_path_s;
+			}
+
+			int too_close = 0;			
+
+			//Go through each object in sensor fusion list...
+			for( int i = 0; i < sensor_fusion.size(); i++) {
+
+				//car is in my lane
+				float d = sensor_fusion[i][6];
+
+				//Only process if in my lane range
+				if( d < (2+4*lane+2) && d > (2+4*lane-2) ) {
+					double vx = sensor_fusion[i][3];
+					double vy = sensor_fusion[i][4];
+					double check_speed = sqrt(vx*vx+vy*vy);   //speed of car in m lane
+					double check_car_s = sensor_fusion[i][5]; //s position of car in my lane
+	
+				    check_car_s+=((double)prev_size*.02*check_speed); //project s points out of car in my lane
+					//See if too close in same lane
+					if( (check_car_s > car_s) && ((check_car_s-car_s) < safeDistDetectionVal) ) {
+						//ref_vel = 29.5;
+						too_close = 1;
+						if( (check_car_s-car_s) < 9 )
+							too_close = 2;
+						else if( (check_car_s-car_s) < 5 )
+							too_close = 3;
+					}
+				}//if
+			}//for
+
+
+			if( too_close > 0 ) { //decelerate and maybe change lanes
+				switch( too_close ) {
+				case 1:
+					ref_vel -= .224; //normal deceleration
+				case 2:
+					ref_vel -= .45;  //medium deceleration
+				case 3:
+					ref_vel -= 1.00; //high deceleration
+				}
+								
+				lane = cost_function1(sensor_fusion, car_s, car_speed, lane);
+				//cout << "lane to go in to=" << lane << endl;				
+
+			}
+			else if( ref_vel < 49.5) { //Accelerate
+				ref_vel += .33; //.224
+			}
+
+
+			//Figure out what points to send back to the simulator....
+
+			vector<double> ptsx;
+			vector<double> ptsy;
+
+			double ref_x = car_x;
+			double ref_y = car_y;
+			double ref_yaw = deg2rad(car_yaw);
+
+
+			//Create or use points to create spline...
+			
+			if(prev_size < 2) //Create a couple previous points then "create" some based on current car_yaw and position
+			{
+				double prev_car_x = car_x - cos(car_yaw);
+				double prev_car_y = car_y - sin(car_yaw);
+
+				ptsx.push_back(prev_car_x);
+				ptsx.push_back(car_x);
+
+				ptsy.push_back(prev_car_y);
+				ptsy.push_back(car_y);
+			}
+			else  //use previous path points
+			{
+				ref_x = previous_path_x[prev_size-1];
+				ref_y = previous_path_y[prev_size-1];
+
+				double ref_x_prev = previous_path_x[prev_size-2];
+				double ref_y_prev = previous_path_y[prev_size-2];
+				ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev);
+
+                ptsx.push_back(ref_x_prev);
+				ptsx.push_back(ref_x);
+
+				ptsy.push_back(ref_y_prev);
+				ptsy.push_back(ref_y);
+				
+			}
+
+			//Make some points out in 30,60, and 90 meters in the distance
+			vector<double> next_mp0 = getXY(car_s+30,(2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+			vector<double> next_mp1 = getXY(car_s+60,(2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+			vector<double> next_mp2 = getXY(car_s+90,(2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+			ptsx.push_back(next_mp0[0]);
+			ptsx.push_back(next_mp1[0]);
+			ptsx.push_back(next_mp2[0]);
+
+			ptsy.push_back(next_mp0[1]);
+			ptsy.push_back(next_mp1[1]);
+			ptsy.push_back(next_mp2[1]);
+
+			//Shift and rotate to origin...
+			for( int i = 0; i < ptsx.size(); i++ ) {
+				double shift_x = ptsx[i] - ref_x;
+				double shift_y = ptsy[i] - ref_y;
+
+				ptsx[i] = (shift_x * cos(0-ref_yaw) - shift_y*sin(0-ref_yaw));
+				ptsy[i] = (shift_x * sin(0-ref_yaw) + shift_y*cos(0-ref_yaw));
+			}
+
+			tk::spline s;
+
+			s.set_points(ptsx,ptsy);  //Set spline points
+
+
+
+			//Set up points to send back to simulator...
 
           	vector<double> next_x_vals;
           	vector<double> next_y_vals;
 
+			//Init with unused previous path points
+			for(int i = 0; i < previous_path_x.size(); i++)
+            {
+              next_x_vals.push_back(previous_path_x[i]);
+              next_y_vals.push_back(previous_path_y[i]);
+            }
 
-          	// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+			//Break up spline points to get to the desired velocity.  This is mainly taken from code walk through.
+			double target_x = 30;
+			double target_y = s(target_x); //y value from spline
+			double target_dist = sqrt((target_x*target_x)+(target_y*target_y));
+
+			double x_add_on = 0;
+
+			//Add some new points to end derived from the spline...
+			for( int i = 1; i <= 50-previous_path_x.size(); i++ ) {
+				double N = (target_dist/(.02*ref_vel/2.24));  //will help us get right spacing
+				double x_point = x_add_on+(target_x)/N;
+				double y_point = s(x_point);
+
+				x_add_on = x_point;
+
+				//Shift and rotate back
+				double x_ref = x_point;
+				double y_ref = y_point;
+
+				x_point = (x_ref*cos(ref_yaw)-y_ref*sin(ref_yaw));
+				y_point = (x_ref*sin(ref_yaw)+y_ref*cos(ref_yaw));
+
+				x_point += ref_x;
+				y_point += ref_y;
+
+				next_x_vals.push_back(x_point);
+				next_y_vals.push_back(y_point);
+
+			}
+
+
+
+		//Drive in a circle
+		 /* double pos_x;
+          double pos_y;
+          double angle;
+          int path_size = previous_path_x.size();
+
+          for(int i = 0; i < path_size; i++)
+          {
+              next_x_vals.push_back(previous_path_x[i]);
+              next_y_vals.push_back(previous_path_y[i]);
+          }
+
+          if(path_size == 0)
+          {
+              pos_x = car_x;
+              pos_y = car_y;
+              angle = deg2rad(car_yaw);
+          }
+          else
+          {
+              pos_x = previous_path_x[path_size-1];
+              pos_y = previous_path_y[path_size-1];
+
+              double pos_x2 = previous_path_x[path_size-2];
+              double pos_y2 = previous_path_y[path_size-2];
+              angle = atan2(pos_y-pos_y2,pos_x-pos_x2);
+          }
+
+          double dist_inc = 0.5;
+          for(int i = 0; i < 50-path_size; i++)
+          {    
+              next_x_vals.push_back(pos_x+(dist_inc)*cos(angle+(i+1)*(pi()/100)));
+              next_y_vals.push_back(pos_y+(dist_inc)*sin(angle+(i+1)*(pi()/100)));
+              pos_x += (dist_inc)*cos(angle+(i+1)*(pi()/100));
+              pos_y += (dist_inc)*sin(angle+(i+1)*(pi()/100));
+          }*/
+
+
+          	
+			json msgJson;
           	msgJson["next_x"] = next_x_vals;
           	msgJson["next_y"] = next_y_vals;
 
